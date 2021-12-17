ada4688ce7f867d5dc99fe2044f877626cc8b4aa
julianadepp <julianadepp7@gmail.com>
2021-12-16 23:44:16 -0500

    fixed cors bug

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const cors = require('cors')
 
 const app = express();
+app.use(cors())
 
 app.set('port', process.env.PORT || 8000)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors)
 
 const employeeController = require('./controllers/employee')
 app.use('/', employeeController)
